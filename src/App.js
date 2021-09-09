f59f096aca63183ea434cf49d38f3d98e0b84ba0
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-09-09 08:25:45 -0400

    Create my rents page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Checkout } from "./pages/Checkout/Checkout";
 import { Home } from "./pages/Home/Home";
+import { MyRents } from "./pages/MyRents/MyRents";
 
 function App() {
   return (
     <Container className="p-3">
       <BrowserRouter>
+      <Route path="/checkout" exact component={Checkout} />
+        <Route path="/my-rents" exact component={MyRents} />
         <Route path="/" exact component={Home} />
-        <Route path="/checkout" exact component={Checkout} />
       </BrowserRouter>
     </Container>
   );
