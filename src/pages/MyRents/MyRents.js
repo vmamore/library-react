7f10cccba7a53255b36f20608ffeaed8da99b5f1
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-09-09 23:27:39 -0400

    Update component to fetch from api

--- src/pages/MyRents/MyRents.js
+++ src/pages/MyRents/MyRents.js
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import { ListGroup } from "react-bootstrap";
+import { fetchAllRentsFromLocator } from "../../services/library-api";
+
 export function MyRents() {
-    return <h1>These are all my rents!</h1>
+  const [bookRentals, setBookRentals] = useState({
+    state: "idle",
+    data: {},
+  });
+
+  useEffect(() => {
+    getAllRentalsFromLocator();
+  }, []);
+
+  async function getAllRentalsFromLocator() {
+    fetchAllRentsFromLocator("3e8eefd3-5f27-46f0-9381-7d1e1a80d2a1").then(
+      (b) => {
+        setBookRentals({
+          state: "resolved",
+          data: b.data,
+        });
+      }
+    );
+  }
+
+  if (bookRentals.state === "idle") return <h1>Loading...</h1>;
+
+  return (
+    <ListGroup variant="flush" className="mb-3">
+      <ListGroup.Item key={bookRentals.data.id}>
+        {bookRentals.data.rentedDay} - {bookRentals.data.status}
+      </ListGroup.Item>
+    </ListGroup>
+  );
 }
