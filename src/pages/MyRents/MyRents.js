a35b9adc64b676f7c1ce0806535624b28e14be37
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-09-12 16:40:46 -0400

    Add new page for show all reants

--- src/pages/MyRents/MyRents.js
+++ src/pages/MyRents/MyRents.js
@@ -1,8 +1,21 @@
 import { useEffect, useState } from "react";
-import { ListGroup } from "react-bootstrap";
+import { Accordion, ListGroup, Badge, Row, Col, Button } from "react-bootstrap";
+import { useHistory } from "react-router";
 import { fetchAllRentsFromLocator } from "../../services/library-api";
+import { BsBook, BsFillBagFill } from "react-icons/bs";
+
+function convertDate(inputDate) {
+  const date = new Date(inputDate);
+
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+}
 
 export function MyRents() {
+  const history = useHistory();
   const [bookRentals, setBookRentals] = useState({
     state: "idle",
     data: {},
@@ -23,13 +36,63 @@ export function MyRents() {
     );
   }
 
+  function goToCatalog() {
+    history.push("/");
+  }
+
   if (bookRentals.state === "idle") return <h1>Loading...</h1>;
 
   return (
-    <ListGroup variant="flush" className="mb-3">
-      <ListGroup.Item key={bookRentals.data.id}>
-        {bookRentals.data.rentedDay} - {bookRentals.data.status}
-      </ListGroup.Item>
-    </ListGroup>
+    <>
+      <h1>
+        My Rentals <BsFillBagFill />
+      </h1>
+      <Accordion defaultActiveKey="0" flush>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            <Badge style={{ marginRight: "35px" }} pill bg="success">
+              {bookRentals.data.status}
+            </Badge>
+            <Badge style={{ marginRight: "35px" }} pill bg="primary">
+              Rented Day: {convertDate(bookRentals.data.rentedDay)}
+            </Badge>
+            <Badge style={{ marginRight: "35px" }} pill bg="warning">
+              Day to Return: {convertDate(bookRentals.data.dayToReturn)}
+            </Badge>
+            <Badge style={{ marginRight: "35px" }} pill bg="info">
+              Librarian: {bookRentals.data.librarianName}
+            </Badge>
+          </Accordion.Header>
+          <Accordion.Body>
+            <ListGroup variant="flush">
+              <h4>Rented Books</h4>
+              {bookRentals.data.books.map((book, index) => (
+                <>
+                  <ListGroup.Item eventKey={index}>
+                    <BsBook /> <b>{book.title} </b>
+                    <Badge pill bg="secondary">
+                      {book.author}
+                    </Badge>
+                  </ListGroup.Item>
+                </>
+              ))}
+            </ListGroup>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <hr />
+      <Row>
+        <Col md={4}>
+          <Button
+            variant="outline-success"
+            onClick={() => {
+              goToCatalog();
+            }}
+          >
+            See Book Catalog
+          </Button>
+        </Col>
+      </Row>
+    </>
   );
 }
